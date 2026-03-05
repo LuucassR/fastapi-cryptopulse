d718e61cd9ba776d0a92a5488b42f3a8e3d01633
Cookie <lucassrossi12@gmail.com>
2026-03-05 17:12:11 -0300

    Delete cors link

--- backend/main.py
+++ backend/main.py
@@ -65,7 +65,6 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://fastapi-cryptopulse-qoh3-git-main-luucassrs-projects.vercel.app/",
-    "https://fastapi-cryptopulse-qoh3.vercel.app/"
 ]
 
 # Utilizamos los middlewares para aceptar los CORS
